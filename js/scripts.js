56e6065081489e59853daeff2abc75a85320fee1
Tia Weyrauch <tiayrock@gmial.com>
2014-05-12 13:46:42 -0700

    version 4
    video

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 // My Scripts
 $(document).foundation();
+
+
+//Big Video
+$(function() {
+    var BV = new $.BigVideo();
+    BV.init();
+    BV.show('http://tweyrauch.github.io/doh/ceans.mp4');
+});
